8b4f6a8a408dc4fe7baf17179e961dc860a393c1
Aravind Murali <aravindmurali711@gmail.com>
2019-01-27 19:02:09 +0530

    Create maximum_remainder_SOL.cpp

---
+++ modulus/maximum_remainder/maximum_remainder_SOL.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+# include <cmath>
+
+int main() {
+	int N;
+	int M;
+	int x;
+	
+	std::cout << "Enter the value of N to find out maximum remainder : ";
+	std::cin >> N;
+	
+	x = ceil((N + 1) / 2);
+	M = floor((N - 1) / 2);
+	std::cout << "Maximum remainder is obtained when divided by : " << x << std::endl;
+	std::cout << "Maximum remainder obtained for the value N is : " << M << std::endl;
+
+  return 0;
+}
