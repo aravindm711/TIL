4385096f3ece0e39ab459ca039f14abde98e5ab8
Aravind Murali <aravindmurali711@gmail.com>
2019-01-31 23:26:54 +0530

    Update TEMPLATE.cpp

--- TEMPLATE.cpp
+++ TEMPLATE.cpp
@@ -19,22 +19,22 @@
 
 using namespace std;
 
-# define cint(d) 			scanf("%d", &d)
+# define cint(d) 		scanf("%d", &d)
 # define cint2(a, b) 		scanf("%d %d", &a, &b)
 # define cint3(a, b, c) 	scanf("%d %d %d", &a, &b, &c)
 # define cint4(a, b, c, d) 	scanf("%d %d %d %d", &a, &b, &c, &d)
 
-# define clong(d) 			scanf("%lld", &d)
+# define clong(d) 		scanf("%lld", &d)
 # define clong2(a, b) 		scanf("%lld %lld", &a, &b)
 # define clong3(a, b, c) 	scanf("%lld %lld %lld", &a, &b, &c)
-# define clong4(a, b, c, d) scanf("%lld %lld %lld %lld", &a, &b, &c, &d)
+# define clong4(a, b, c, d) 	scanf("%lld %lld %lld %lld", &a, &b, &c, &d)
 
-# define pint1(d) 			printf("%d", d)
+# define pint1(d) 		printf("%d", d)
 # define pint2(a, b) 		printf("%d %d", a, b)
 # define pint3(a, b, c)		printf("%d %d %d", a, b, c)
 # define pint4(a, b, c, d)	printf("%d %d %d %d", a, b, c, d)
 
-# define plong(d)			printf("%lld", d)
+# define plong(d)		printf("%lld", d)
 # define plong2(a, b)		printf("%lld %lld", a, b)
 # define plong3(a, b, c)	printf("%lld %lld %lld", a, b, c)
 # define plong4(a, b, c, d)	printf("%lld %lld %lld %lld", a, b, c, d)
@@ -65,6 +65,7 @@ using namespace std;
 # define mset(m,v)              memset(m,v,sizeof(m))
 # define INF                    0x3f3f3f3f
 # define INFL                   0x3f3f3f3f3f3f3f3fLL
+
 typedef long long              int64;
 typedef pair<int,int>          p32;
 typedef pair<int64,int64>      p64;
@@ -76,8 +77,9 @@ typedef vector<p64>            vp64;
 typedef vector<p32>            vp32;
 typedef map<int,int>           m32;
 typedef map<int64,int64>       m64;
+
 //#pragma GCC optimize ("-O2")
-//template<class T> using min_heap = priority_queue<T, std::vector<T>, std::greater<T> >;
+
 const int MOD = 1000000007;
 # define MODSET(d) 			if ((d) >= MOD) d %= MOD;
 # define MODNEGSET(d) 			if ((d) < 0) d = ((d % MOD) + MOD) % MOD;
@@ -92,7 +94,7 @@ const int INT_INFINITY = 1001001001;
 const double EPS = 1e-6;
 const int LIMIT = 1e6;
 
-# define looptc int t; cin >> t; while(t--)
+# define looptc 		int t; cin >> t; while(t--)
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
