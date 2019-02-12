16b1688e726a09b9c693e38dd89b0467e67e62f0
Aravind Murali <aravindmurali711@gmail.com>
2019-02-12 16:55:34 +0530

    Update looping_through_bits.cpp

--- bitmasks/looping_through_bits.cpp
+++ bitmasks/looping_through_bits.cpp
@@ -29,13 +29,13 @@ int main() {
 
 	int a = 34;
   
-  cout << (a << 1) << endl;       // multiplying the number by 2
+  	cout << (a << 1) << endl;       // multiplying the number by 2
   
-  cout << (a >> 2) << endl;       // dividing the number by 4 (will give the integer division result)
+  	cout << (a >> 2) << endl;       // dividing the number by 4 (will give the integer division result)
   
-  cout << (a | (1 << 3)) << endl; // changing the 4th bit from 0 to 1
+  	cout << (a | (1 << 3)) << endl; // changing the 4th bit from 0 to 1
   
-  cout << (a & (1 << 3)) << endl; // checking if the jth bit is on or off (0 if 0, non-zero if 1)
+  	cout << (a & (1 << 3)) << endl; // checking if the jth bit is on or off (0 if 0, non-zero if 1)
 
 	return 0;
 }
